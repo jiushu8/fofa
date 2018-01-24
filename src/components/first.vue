15a42046b8b8ee79be150654a15a34a714d25b6b
niuhongliang <hongliang.niu@nexguard.com>
2018-01-24 16:55:40 +0800

    all

---
+++ src/components/first.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h2>大爱无疆</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'first'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  font-size:46px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import first from '@/components/first'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/abc',
+      name: 'first',
+      component: first
+    }
+  ]
+})
